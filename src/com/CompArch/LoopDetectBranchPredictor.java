dbadfa09719afe78f6a0a4c4a595ed266241bfd2
jmd1080 <jd9993@bristol.ac.uk>
2012-12-11 23:25:31 +0000

    Added Loop Detecting Branch Predictor

---
+++ src/com/CompArch/LoopDetectBranchPredictor.java
@@ -0,0 +1,53 @@
+package com.CompArch;
+
+// Identifies loops by the incrementing that occurs before the jump
+
+public class LoopDetectBranchPredictor extends BranchPredictor{
+
+	int count; // Cycles since a value was last incremented
+	int lag;
+	
+	LoopDetectBranchPredictor(Simulator s) {
+		super(s);
+	}
+
+	@Override
+	boolean branches(int[] instruct) {
+		return count > 0;
+	}
+
+	@Override
+	void tick() {
+	}
+	
+	public void setLag(int l)
+	{
+		lag = l;
+	}
+	
+	
+	/* Returns true if an instruction is an increment. 
+	** Defined as an add or addi where destination register
+	** is one of the parameters*/
+	private boolean isIncrement (int[] instruct)
+	{
+		if (instruct[0] != 3 && instruct[0] != 4)
+			return false;
+		else if (instruct[1] == instruct[2])
+			return true;
+		else if (instruct[0] == 4 && instruct[1] == instruct[3])
+			return true;
+		
+		return false;
+	}
+
+	@Override
+	void parseInstruct(int[] instruct) {
+		
+		lag--;
+		
+		if (isIncrement(instruct))
+			count = lag; 
+	}
+
+}
