168742a9cdc9c66eb4760a6696bbf5af6767ecb4
jmd1080 <jd9993@bristol.ac.uk>
2012-12-14 03:29:43 +0000

    Added test for parallel processing

---
+++ paralleltest.asm
@@ -0,0 +1,14 @@
+//Adding stage (14 cycles)
+// Result:
+addim 1 1 2
+addim 2 1 18
+div 5 2 1
+div 4 1 1
+div 3 1 1
+div 4 1 1
+//Memory writing
+write 1 0 0
+write 2 0 1
+write 3 0 2
+write 5 0 3
+write 4 0 3
